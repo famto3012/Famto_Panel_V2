6a4860d072afb45097feda18c9ff24c8f177e6a6
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-12-13 13:16:26 +0530

    Ui updates

--- src/screens/appCustomization/merchantApp/MerchantApp.jsx
+++ src/screens/appCustomization/merchantApp/MerchantApp.jsx
@@ -3,7 +3,7 @@ import GlobalSearch from "../../../components/others/GlobalSearch";
 
 const MerchantApp = () => {
   return (
-    <div className="bg-gray-100 h-full">
+    <div className="bg-gray-100 min-h-full">
       <GlobalSearch />
       <Toggles />
     </div>
