0998a592b357769a5d0604260c02e65012dc0c6c
its-sarath28 <sarathrs220@gmail.com>
2024-12-14 16:05:39 +0530

    Adjusted Address selection in P&D

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import CustomRouter from "./routes/CustomRouter.jsx";
+import CustomRouter from "@/routes/CustomRouter.jsx";
 import { Provider } from "@/components/ui/provider";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "@/context/AuthContext.jsx";
+import { AddressProvider } from "@/context/AddressContext";
 
 const queryClient = new QueryClient();
 
@@ -17,9 +18,11 @@ createRoot(document.getElementById("root")).render(
     <Toaster />
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <CustomRouter>
-          <App />
-        </CustomRouter>
+        <AddressProvider>
+          <CustomRouter>
+            <App />
+          </CustomRouter>
+        </AddressProvider>
       </AuthProvider>
     </QueryClientProvider>
   </Provider>
