29fc101cb6187c724e610e5004687c36a54f138e
its-sarath28 <sarathrs220@gmail.com>
2024-12-23 13:42:44 +0530

    Bug fix

--- src/routes/HomeRoute.jsx
+++ src/routes/HomeRoute.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Loader from "../components/others/Loader";
 
-const Home = lazy(() => import("../screens/general/Home/Home"));
+const Home = lazy(() => import("../screens/general/home/Home"));
 
 const HomeRoutes = () => (
   <Suspense fallback={<Loader />}>
